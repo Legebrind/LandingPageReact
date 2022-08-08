7bb98d05421b5cc3d78aa342230c20806a58e282
Legebrind <moyi88@gmail.com>
2022-08-08 18:40:08 +0000

    Added Home

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,8 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-import NavBar from "./component/navbar.jsx";
-import Jumbotron from "./component/jumbotron.jsx";
-import Cardholder from "./component/cardsholder.jsx";
-import Footer from "./component/footer.jsx";
+
 
 
 //render your react application
-ReactDOM.render([<NavBar />,<Jumbotron />,<Cardholder />,<Footer/>], document.querySelector("#app"));
+ReactDOM.render(<Home/>, document.querySelector("#app"));
